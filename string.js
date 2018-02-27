e0c3d7491a0bb2501d078d12e24d4f4f5b2e2767
Rita Patrick <rita.patrick1007@gmail.com>
2018-02-26 20:41:59 -0600

    completed exercise 3

--- string.js
+++ string.js
@@ -4,6 +4,15 @@ var numOfSandwhiches=4*2;
 
 console.log("numOfSandwhiches", numOfSandwhiches);
 
+// challange #2
+
 var name = "Rita";
 var string = "Hello, " + name + " how are you doing today?";
 console.log("string:", string);
+
+// challange #3
+
+var DNA= "GCAT";
+var RNA = DNA.replace('T', 'U');
+var RNA = DNA.replace(/T/g, 'U');
+console.log("RNA", RNA);
