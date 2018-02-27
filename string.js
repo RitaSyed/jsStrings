e542ebd76e249efaad3414d51adbf1d4a7511d73
Rita Patrick <rita.patrick1007@gmail.com>
2018-02-26 21:31:31 -0600

    added innerHTML info

--- string.js
+++ string.js
@@ -14,7 +14,7 @@ console.log("string:", string);
 
 var DNA= "GCAT";
 var RNA = DNA.replace('T', 'U');
-var RNA = DNA.replace(/T/g, 'U');
+// var RNA = DNA.replace(/T/g, 'U');
 console.log("RNA", RNA);
 
 // challange #4
@@ -32,3 +32,18 @@ if (animal.toLowerCase() == "alligator"){
 } else {
   console.log("wide")
 }
+
+var animal = "AlliGATor";
+if (animal.toLowerCase() == "alligator"){
+
+  console.log("small")
+} else {
+  console.log("wide")
+}
+
+
+var yarn = "the better string";
+var domString = "<h4>" + yarn + "</h4"
+
+var myDiv = document.getElementById("yarn-holder");
+myDiv.innerHTML = domString;
