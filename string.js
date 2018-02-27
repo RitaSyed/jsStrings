44d1119826c185e9bc4a9598c07db9d0d55d53d1
Rita Patrick <rita.patrick1007@gmail.com>
2018-02-26 20:30:53 -0600

    completed exercise 2

--- string.js
+++ string.js
@@ -3,3 +3,7 @@ console.log ("Hello");
 var numOfSandwhiches=4*2;
 
 console.log("numOfSandwhiches", numOfSandwhiches);
+
+var name = "Rita";
+var string = "Hello, " + name + " how are you doing today?";
+console.log("string:", string);
